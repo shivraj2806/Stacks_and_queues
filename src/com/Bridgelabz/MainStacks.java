1736966ee8cf52c4bd980be421f8a9e3c9ed0d2f
shivraj2806 <shivjadhav2806@gmail.com>
2021-08-29 13:55:09 +0530

    Insert a element in the stacks

---
+++ src/com/Bridgelabz/MainStacks.java
@@ -0,0 +1,15 @@
+package com.Bridgelabz;
+
+public class MainStacks {
+        public static void main(String[] args) {
+            Stacks stack = new Stacks();
+            stack.push(70);
+            stack.push(30);
+            stack.push(56);
+            stack.display();
+
+        }
+    }
+
+
+
--- src/com/Bridgelabz/Stacks.java
+++ src/com/Bridgelabz/Stacks.java
@@ -1,7 +1,45 @@
 package com.Bridgelabz;
-
 public class Stacks {
-    public static void main(String[] args) {
-        System.out.println("welcome to stacks and queues program");
+    Node top;
+
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
     }
+
+    public boolean isEmpty() {
+        if (top == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //****************************UC1 Create a stacks of 56->30->70**************************
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if (top == null) {
+            top = newNode;
+        } else {
+            Node temp = top;
+            top = newNode;
+            newNode.next = temp;
+        }
+        System.out.println("Element Push: " + top.data);
+    }
+
+    public void display() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+    }
+
+
 }
