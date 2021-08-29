fa06630d344ae4adbe7534107010896374a72aed
shivraj2806 <shivjadhav2806@gmail.com>
2021-08-29 13:44:18 +0530

    welcome message

--- src/com/Bridgelabz/Stacks.java
+++ src/com/Bridgelabz/Stacks.java
@@ -2,7 +2,6 @@ package com.Bridgelabz;
 
 public class Stacks {
     public static void main(String[] args) {
-
-        System.out.println("Welcome to stacks and queues");
+        System.out.println("welcome to stacks and queues program");
     }
-    }
+}
