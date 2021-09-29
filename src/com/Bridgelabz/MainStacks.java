97332c2dfd2fda4884b9a11d6263ac0eba3de55d
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-29 17:28:04 +0530

    Added: peak and pop method

--- src/com/Bridgelabz/MainStacks.java
+++ src/com/Bridgelabz/MainStacks.java
@@ -1,15 +1,21 @@
 package com.Bridgelabz;
 
 public class MainStacks {
-        public static void main(String[] args) {
-            Stacks stack = new Stacks();
-            stack.push(70);
-            stack.push(30);
-            stack.push(56);
-            stack.display();
+    public static void main(String[] args) {
 
-        }
+        Stacks stack = new Stacks();
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+
+        stack.display();
+
+        System.out.println(stack.peak());
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        stack.display();
     }
-
-
+}
 
--- src/com/Bridgelabz/Stacks.java
+++ src/com/Bridgelabz/Stacks.java
@@ -19,7 +19,6 @@ public class Stacks {
             return false;
         }
     }
-
     //****************************UC1 Create a stacks of 56->30->70**************************
     public void push(int data) {
         Node newNode = new Node(data);
@@ -33,13 +32,34 @@ public class Stacks {
         System.out.println("Element Push: " + top.data);
     }
 
+//**************************UC2 pop from the stacks**************************************************
+    public int peak() {
+        if (isEmpty()) {
+            System.out.println("Empty Stack");
+            return -1;
+        }
+        return top.data;
+    }
+
+    public int pop() {
+        int poped;
+
+        if (isEmpty()) {
+            System.out.println("Empty Stack.");
+            return -1;
+        }
+        poped = top.data;
+        top = top.next;
+
+        return poped;
+    }
+
     public void display() {
         Node temp = top;
         while (temp != null) {
             System.out.print(temp.data + "->");
             temp = temp.next;
         }
+        System.out.println("Null");
     }
-
-
 }
