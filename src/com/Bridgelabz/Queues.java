0c75ee5359415d8e00f4992149dda41b9611b6c7
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-29 21:45:06 +0530

    Added: Dequeue method

--- src/com/Bridgelabz/Queues.java
+++ src/com/Bridgelabz/Queues.java
@@ -2,50 +2,60 @@ package com.Bridgelabz;
 public class Queues {
 
     public class Node {
-            int data;
-            Node next;
+        int data;
+        Node next;
 
-            Node(int data) {
-                this.data = data;
-            }
-        }
-
-        Node front, rear;
-
-        Queues() {
-            this.front = null;
-            this.rear = null;
-        }
-
-        public void enQueue(int data) {
-            Node newNode = new Node(data);
-            if (this.rear == null) {
-                this.front = newNode;
-                this.rear = newNode;
-                return;
-            }
-            this.rear.next = newNode;
-            this.rear = newNode;
-            System.out.println("Element is Add: " + newNode.data);
-        }
-
-        public int size() {
-            int count = 0;
-            Node temp = this.front;
-            while (temp != null) {
-                count++;
-                temp = temp.next;
-            }
-            return count;
-        }
-
-        public void display() {
-            Node temp = front;
-            while (temp != null) {
-                System.out.print(temp.data + "->");
-                temp = temp.next;
-            }
-            System.out.println("Null");
+        Node(int data) {
+            this.data = data;
         }
     }
 
+    Node front, rear;
+
+    Queues() {
+        this.front = null;
+        this.rear = null;
+    }
+  // UC3 Ability to create Queue of 56,30 and 70.
+    public void enQueue(int data) {
+        Node newNode = new Node(data);
+        if (this.rear == null) {
+            this.front = newNode;
+            this.rear = newNode;
+            return;
+        }
+        this.rear.next = newNode;
+        this.rear = newNode;
+        System.out.println("Element is Add: " + newNode.data);
+    }
+
+    public int size() {
+        int count = 0;
+        Node temp = this.front;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    // UC4 Ability to dequeue from the begining.
+    public int deQueue() {
+        if (this.front == null) {
+            System.out.println("Empty Queue.");
+            return -1;
+        }
+        Node temp = this.front;
+        this.front = this.front.next;
+        return temp.data;
+    }
+
+    public void display() {
+        Node temp = front;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+}
--- src/com/Bridgelabz/Queues_main.java
+++ src/com/Bridgelabz/Queues_main.java
@@ -2,13 +2,18 @@ package com.Bridgelabz;
 
 public class Queues_main {
     public static void main(String[] args) {
-          Queues queue = new Queues();
-              queue.enQueue(56);
-              queue.enQueue(30);
-              queue.enQueue(70);
-              queue.display();
-     }
-   }
+        Queues queue = new Queues();
+        queue.enQueue(56);
+        queue.enQueue(30);
+        queue.enQueue(70);
+        queue.display();
+
+        System.out.println(queue.deQueue());
+        System.out.println(queue.deQueue());
+        System.out.println(queue.deQueue());
+        queue.display();
+    }
+}
 
 
 
